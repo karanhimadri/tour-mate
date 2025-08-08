4367b9feb41f52047420837076a9ea21fc65bbf2
AnishCoding2004 <anishkar2606@gmail.com>
2025-08-09 02:12:45 +0530

    anish Change data.js

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,4 +1,4 @@
-const homeyHutsCategories = [
+/*const homeyHutsCategories = [
   "Featured",
   "Mountain View",
   "Villa",
@@ -67,3 +67,266 @@ const homeyHutsListings = [
 ];
 
 export { homeyHutsCategories, homeyHutsListings };
+*/
+const MountainView = [
+  {
+    title: "Jungle Kinare – Sound of nature - Mountain Views",
+    location: "Sunderkhal Mukteshwar, Uttarakhand, India",
+    images: [
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1099/image_kewok_9_28_2024_12%3A52%3A46_PM_656_450.webp",
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1099/image_86e56_9_28_2024_12%3A58%3A37_PM.webp",
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1099/image_ockwx_4_24_2025_10%3A45%3A09_AM.webp",
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1099/image_dvobuj_9_28_2024_12%3A53%3A46_PM.webp",
+    ],
+    homastay_capacity: {
+      guests: 8,
+      bedrooms: 3,
+      beds: 3,
+      bathrooms: 3
+    },
+    hosted_by: "Anshu",
+    uniqueness: [
+      "Mountain View", "Family Friendly", "Eco-Friendly", "Yoga/Meditation", "Countryside Views", "Peaceful Retreat"
+    ],
+    about: "Nestled in the peaceful hills of Mukteshwar, Jungle Kinare is a cozy 3-bedroom cottage surrounded by lush apple orchards. This charming retreat is an ideal escape for couples, families, and solo travelers seeking a break from the daily hustle.",
+    offers: ["Wifi", "Free Parking On Premises", "Sound System", "Kitchen", "Fire Extinguisher", "Garden View"],
+    actual_price: 8780.4,
+    discounted_price: 6350.4,
+    ratings: 5,
+    reviews: ["Beautiful Property", "Cleanliness", "Nature View", "Good Neighbors"],
+    total_reviews: 6,
+    likes: 2,
+    shares: 10,
+    cancelation_policy: "Cancellation 14 Days Before Check-In: Guest can cancel within 14 days before check-in for a full refund. Cancellations within 14 days or no show will be non-refundable.",
+    hot_price: true
+  },
+  {
+    title: "Blue Magpie – Experience in Local Living",
+    location: "Satkhol, Mukteshwar, Uttarakhand, India",
+    images: [
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1175/image_5266il_10_11_2024_6%3A27%3A59_AM.webp",
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1175/image_ei3mx_10_11_2024_6%3A18%3A04_AM.webp",
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1175/image_6hv75_2_12_2025_6%3A01%3A15_AM.webp",
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1175/image_q91eg_10_11_2024_6%3A19%3A57_AM.webp"
+    ],
+    homastay_capacity: {
+      guests: 4,      
+      bedrooms: 1,
+      beds: 1,
+      bathrooms: 1
+    },
+    hosted_by: "Isha",
+    uniqueness: [
+      "Mountain View", "Family Friendly", "Eco-Friendly","Surrounded by Nature"
+    ],
+    about: "Blue Magpie Homestay is a serene, family-run retreat nestled in the Middle of Himalayas, in the village of Satkhol near Mukteshwar, Uttarakhand. Surrounded by fruit orchards and organic fields, this homestay offers a holistic experience, perfect for nature lovers, work-from-home travelers, and those looking to unwind. It is ideal for guests seeking to live like locals and immerse themselves in the simple, authentic mountain lifestyle , not for those looking for luxury amenities or services.",
+    offers: ["Wifi","TV","Kitchen", "Free Parking On Premises", "Garden/Lawn View"],
+    actual_price: 7317,          // ₹7,317 as per screenshot
+    discounted_price: 5292,
+    ratings: 5,
+    reviews: ["Exceptional service", "Authentic local living"],
+    total_reviews: 1,
+    likes: 6,
+    shares: 2,
+    cancelation_policy: "Flexible cancellation until 7 days before check-in for a full refund.",
+    hot_price: true
+  },
+  {
+    title: "Himalayan Dream Cove by the River",
+    location: "Manali, Himachal Pradesh, India",
+    images: [
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1431/image_if8db_2_12_2025_6%3A50%3A27_AM_656_450.webp",
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1431/image_2oacpf_1_16_2025_6%3A56%3A00_AM.webp",
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1431/image_bpgn5_2_12_2025_6%3A50%3A37_AM.webp",
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1431/image_0zh2g_2_12_2025_6%3A50%3A26_AM.webp"
+
+
+    ],
+    homastay_capacity: {
+      guests: 12,
+      bedrooms: 6,
+      beds: 6,
+      bathrooms: 6
+    },
+    hosted_by: "Siddhi",
+    uniqueness: [
+      "Mountain View", "Riverfront", "Photography-Friendly", "Snow Views", "Adventure Base", "Family Friendly"
+    ],
+    about: "Nestled in the serene landscapes of Manali, Himalayan Dream Cove by the river Cottage offers a peaceful retreat in charming huts surrounded by mesmerizing views of mountains, rivers, and lush valleys. Designed to provide an escape from the chaos of daily life, this cozy property is perfect for couples, small families, and groups seeking relaxation amidst nature’s tranquility.",
+    offers: ["Wifi", "Free Parking On Premises", "Kitchen", "Heating","Fire Extinguisher", "Mountain View"],
+    actual_price: 19975.24,
+    discounted_price: 14653.24,
+    ratings: 5,
+    reviews: ["Scenic and peaceful", "Spacious and comfortable"],
+    total_reviews: 9,
+    likes: 6,
+    shares: 9,
+    cancelation_policy: "Cancellation allowed before 14 days for full refund.",
+    hot_price: true
+  },
+  {
+    title: "Surfing Yogis – 1BR Luxe Cottage in Puri",
+    location: "Balighai, Puri, Odisha, India",
+    images: [
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1648/image_23fpj_3_7_2025_12%3A19%3A04_PM_656_450.webp",
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1648/image_9axb5_3_7_2025_12%3A19%3A29_PM.webp",
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1648/image_slv9b_5_8_2025_12%3A25%3A49_PM.webp",
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1648/image_5my33_5_8_2025_12%3A25%3A06_PM.webp"
+    ],
+    homastay_capacity: {
+      guests: 5,
+      bedrooms: 1,
+      beds: 2,
+      bathrooms: 1
+    },
+    hosted_by: " Isha",
+    uniqueness: [
+      "Beach House", "Eco-Friendly", "Ocean Front", "Surrounded by Nature"
+    ],
+    about: "Welcome to Your Serene Surfside Escape – A Hidden Gem Near Beleswar Beachh\nThis isn’t just a place to crash — it’s where the surf community comes together in Puri. Run by surfers who live for the ocean, our place is made for people who’d rather spend sunrise in the water than in bed.\n\nWe’ve got a surf school right on site, with boards, lessons, and everything you need whether you're just starting out or chasing your next big wave. The beach is right there, 15 min walk through a mesmerising walking trail.\n\nwhen the sea is calm, we take to the nearby river with paddles and kayaks. It’s all about staying close to the water.",
+    offers: ["Wifi", "AC", "Free Parking On Premises", "Beach Access", "Washing Machine", "Fire Extinguisher"],
+    actual_price: 20268,
+    discounted_price: 14868,
+    ratings: 5,
+    reviews: ["Amazing hosts!", "Great architecture and location."],
+    total_reviews: 10,
+    likes: 4,
+    shares: 2,
+    cancelation_policy: "Full refund for cancellations up to 5 days before check-in.",
+    hot_price: true
+  },
+   {
+    title: "Cedar Oak Cottage 1BR – New",
+    location: "Mashyar, Himachal Pradesh, India",
+    images: [
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1424/image_kec82_2_18_2025_5%3A42%3A22_AM_656_450.webp",
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1423/image_q4ufo_1_9_2025_5%3A10%3A57_AM.webp",
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1423/image_9tzud_1_9_2025_5%3A12%3A40_AM.webp",
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1423/image_c69xo_1_9_2025_5%3A10%3A26_AM.webp"
+    ],
+    homastay_capacity: {
+      guests: 3,
+      bedrooms: 1,
+      beds: 1,
+      bathrooms: 1
+    },
+    hosted_by: "Yash",
+    uniqueness: [
+      "Mountain View", "Countryside Views", "Peaceful Retreat","Romantic Getaway","Adventure Base"
+    ],
+    about: "Cedar is a secluded mountain retreat it's a 1 Bedrooms cottage offering a serene escape with a spacious One double bedroom and an attached bathroom, perfect for couples or small families. With a maximum occupancy of 3 guests, it’s an ideal getaway for those seeking tranquility, adventure, and a chance to unwind amidst nature. Enjoy breathtaking mountain views, peaceful surroundings, and an array of outdoor activities like hiking and trekking.",
+    offers: ["Kitchen", "Free Parking On Premises", "Garden View","Fire Extinguisher","First Aid Kit","Mountain View"],
+    actual_price: 3707.28,
+    discounted_price: 2681.28,
+    ratings: 5,
+    reviews: ["Cozy interiors", "Peaceful surroundings"],
+    total_reviews: 2,
+    likes: 9,
+    shares: 2,
+    cancelation_policy: "Full refund for cancellations up to 7 days before check-in.",
+    hot_price: true
+  },
+  {
+    title: "Nature's Paradise Tirthan: The Pahadi Home",
+    location: "Gushaini, Himachal Pradesh, India",
+    images: [
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1476/image_h9yhwh_1_23_2025_9%3A36%3A24_AM_656_450.webp",
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1476/image_ol4e9_1_23_2025_9%3A49%3A48_AM.webp",
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1476/image_ein9v_1_23_2025_9%3A36%3A23_AM.webp",
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1476/image_mjk67_1_23_2025_9%3A48%3A10_AM.webp",
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1476/image_jjme0f_1_23_2025_9%3A47%3A45_AM.webp"
+    ],
+    homastay_capacity: {
+      guests: 6,
+      bedrooms: 2,
+      beds: 2,
+      bathrooms: 2
+    },
+    hosted_by: "ROHIT",
+    uniqueness: [
+      "Lake Front", "Mountain View", "Peaceful Retreat", "Yoga/Meditation", "Family Friendly", "Countryside Views"
+    ],
+about: "Welcome to Nature's Paradise Tirthan - The Pahadi Ghar.\n\nNestled amidst the serene landscapes of Shairopa, Himachal Pradesh, The Pahadi Ghar is a charming homestay offering a peaceful escape into the lap of nature. This modern yet cozy property features 2 beautifully designed bedrooms and 2 bathrooms, making it perfect for families, groups of friends, and nature lovers looking to rejuvenate.",
+    offers: ["Wifi", "Kitchen", "Fire Extinguisher", "First Aid Kitden View", "Balcony View"],
+    actual_price: 5853.6,
+    discounted_price: 4233.6,
+    ratings: 5,
+    reviews: ["Warm hospitality", "Great mountain views"],
+    total_reviews: 5,
+    likes: 3,
+    shares: 5,
+    cancelation_policy: "Free cancellation before 7 days of check-in.",
+    hot_price: true
+  },
+  {
+    title: "Stone Cottage 2BR | Mukteshwar",
+    location: "Mukteshwar, Uttarakhand, India",
+    images: [
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1499/image_21vuig_2_12_2025_12%3A45%3A31_PM.webp",
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1499/image_gra6r_1_28_2025_9%3A35%3A40_AM.webp",
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1499/image_xszg6_1_28_2025_9%3A33%3A35_AM.webp",
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1499/image_w34wq_1_28_2025_9%3A33%3A36_AM.webp"
+    ],
+    homastay_capacity: {
+      guests: 6,
+      bedrooms: 2,
+      beds: 2,
+      bathrooms: 2
+    },
+    hosted_by: "Isha",
+    uniqueness: [
+      "Mountain View", "Peaceful Retreat", "Family Friendly","Surrounded by Nature","Adventure Base","Romantic Getaway"
+    ],
+about: `Important Information (must read)
+
+• Parking available on the roadside 300 meters from cottage.
+
+• Last 1 km is a gravel road before parking (drivable kacha road).
+
+• 300-meter walk/mild trek from parking to the cottage (luggage assistance provided).`,
+    offers: ["Wifi", "Mountain View", "Garden View","Fire Extinguisher","First Aid Kit","TV"],
+    actual_price: 8780.4,
+    discounted_price: 6350.4,
+    ratings: 4.8,
+    reviews: ["Beautiful stonework", "Calm and relaxing stay"],
+    total_reviews: 4,
+    likes: 5,
+    shares: 4,
+    cancelation_policy: "Full refund if cancelled before 14 days of check-in.",
+    hot_price: true
+  },
+  {
+    title: "RiverSoul Cottage | 2BR with attic bed",
+    location: "Mashyar, Himachal Pradesh, India",
+    images: [
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1516/image_vw302_2_5_2025_10%3A17%3A27_AM.webp",
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1516/image_yeunqh_2_5_2025_10%3A23%3A12_AM.webp",
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1516/image_1kswm_2_5_2025_10%3A18%3A37_AM.webp",
+      "https://homeyhuts-prod-assets.s3.ap-south-1.amazonaws.com/1516/image_9fhyw_2_5_2025_10%3A17%3A43_AM.webp"
+    ],
+    homastay_capacity: {
+      guests: 12,
+      bedrooms: 2,
+      beds: 3,
+      bathrooms: 2
+    },
+    hosted_by: "Sreenandh",
+    uniqueness: [
+      "Mountain View", "Lake Front", "Romantic Getaway", "Surrounded by Nature", "Adventure Base", "Family Friendly"
+    ],
+    about: "Perched in the heart of the scenic Falachan Valley near Tirthan, RiverSoul Cottage is a serene riverside retreat offering breathtaking mountain views and the soothing sound of the river. Designed with traditional wooden architecture, this cozy 2-bedroom attic-style cottage is perfect for groups, families, couples, and nature lovers looking to escape into Peaceful stay.",
+    offers: ["First Aid Kit", "Kitchen", "Free Parking On Premises", "River Front", "Fire Extinguisher", "Mountain View"],
+    actual_price: 6834.56,
+    discounted_price: 4942.56,
+    ratings: 5,
+    reviews: ["Amazing river views", "Spacious rooms"],
+    total_reviews: 20,
+    likes: 10,
+    shares: 8,
+    cancelation_policy: "Guests must cancel at least 5 days before check-in for a full refund. Cancellations within 5 days or no shows will be non refundable.",
+    hot_price: true
+  }
+
+];
+
+export { MountainView };
